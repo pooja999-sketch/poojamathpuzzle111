dcf00e2b0ee83abc095f52b5301f3f37675ced74
godhanimohit <gm.godhanimohit@gmail.com>
2022-12-19 14:07:30 +0530

    123

--- lib/page2.dart
+++ lib/page2.dart
@@ -19,17 +19,98 @@ class _page2State extends State<page2> {
             image: DecorationImage(
                 image: AssetImage("images/gameplaybackground.jpg"),
                 fit: BoxFit.fill)),
-
-        child: Padding(
-          padding:
-              const EdgeInsets.only(left: 20, top: 90, bottom: 320, right: 20),
-          child:
-          Container(
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("images/p1.png"), fit: BoxFit.fill)),
-          ),
-
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 70,
+                      width: 70,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage("images/skip.png"))),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: 70,
+                      width: 100,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage("images/level_board.png"))),
+                      child: Center(
+                          child: Text("Puzzle 1",
+                              style: TextStyle(
+                                  fontSize: 30, color: Colors.black))),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: 70,
+                      width: 70,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage("images/hint.png"))),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: Container(
+                  decoration: BoxDecoration(
+                      image:
+                          DecorationImage(image: AssetImage("images/p1.png"))),
+                ),
+              ),
+            ),
+            Container(
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Expanded(
+                          child: Container(
+                            height: 40,
+                            width: 600,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          height: 40,
+                          width: 150,
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage("images/delete.png"))),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          height: 40,
+                          width: 100,
+                          child: Text("SUBMIT",
+                              style:
+                                  TextStyle(fontSize: 30, color: Colors.white)),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              height: 150,
+              width: double.infinity,
+              color: Colors.black,
+            )
+          ],
         ),
       ),
     );
